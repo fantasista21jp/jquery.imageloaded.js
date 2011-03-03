9bd7418750a88e1a1996408a9b106ae8b8def5bf
fantasista21jp <info@fantasista21jp.com>
2011-03-03 13:53:37 +0900

    first commit

---
+++ js/jquery.imageloaded.js
@@ -0,0 +1,57 @@
+/*
+ * jQuery imageloaded (jQuery Plugin)
+ *
+ * Copyright (c) 2010 Tom Shimada
+ *
+ * Depends Script:
+ *	js/jquery.js (1.3.2~)
+ */
+
+(function($) {
+  $.fn.imageloaded = function(configs) {
+    var defaults = {
+          imgExpression: 'img',
+          beforeFunc: null,
+          afterFunc: null
+        };
+    configs = $.extend(defaults, configs);
+    var selectors = {
+          container: this,
+          imgs: $(configs.imgExpression, this)
+        };
+    if (!selectors.container.length || !selectors.imgs.length) {
+      if ($.isFunction(configs.beforeFunc)) configs.beforeFunc(selectors.container, selectors.imgs);
+      if ($.isFunction(configs.afterFunc)) configs.afterFunc(selectors.container, selectors.imgs);
+      return;
+    }
+
+    if ($.isFunction(configs.beforeFunc)) configs.beforeFunc(selectors.container, selectors.imgs);
+
+    var imgCnt = selectors.imgs.length,
+        loadedCnt = 0,
+        errorCnt = 0,
+        abortCnt = 0;
+    selectors.imgs.each(function(){
+      var $img = $(this),
+          image = new Image();
+      image.src = $img.attr('src');
+      image.onload = function() {
+        loadedCnt++;
+        loaded();
+      }
+      image.onerror = function() {
+        errorCnt++;
+        loaded();
+      }
+      image.onabort = function() {
+        abortCnt++;
+        loaded();
+      }
+    });
+
+    function loaded() {
+      if (imgCnt > (loadedCnt + errorCnt + abortCnt)) return;
+      if ($.isFunction(configs.afterFunc)) configs.afterFunc(selectors.container, selectors.imgs);
+    }
+  }
+})(jQuery);
